f83a4f41f0bbcf8146cff54f1c7b4578bb5934f3
gawakazu <cbb00343@jcom.zaq.ne.jp>
2023-03-16 14:52:06 +0900

    7 commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-_)(3p-_0g@63pw7=tk*_ko2&2w7$sptk$u=nb%lv19@pznz$b#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'e-learning-5nzm.onrender.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
