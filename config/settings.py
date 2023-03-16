5ef2ec4faeefe561ec784a55693bde04ef1ee6da
gawakazu <cbb00343@jcom.zaq.ne.jp>
2023-03-16 15:35:57 +0900

    9 commit

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_)(3p-_0g@63pw7=tk*_ko2&2w7$sptk$u=nb%lv19@pznz$b#
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'e-learning.onrender.com',
+    'e-learning-fm53.onrender.com',
     '127.0.0.1',
 ]
 
