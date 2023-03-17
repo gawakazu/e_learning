3c4379acc9d213814f41c853ab1d58e39886c7a4
gawakazu <cbb00343@jcom.zaq.ne.jp>
2023-03-17 13:10:09 +0900

    18 commit

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
 #STATICFILES_STRAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]##
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
