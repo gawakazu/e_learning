9957c91fd3ac2a52fa7f774326d46dc7b065d648
gawakazu <cbb00343@jcom.zaq.ne.jp>
2023-03-23 13:59:58 +0900

    27 commit

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,8 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_)(3p-_0g@63pw7=tk*_ko2&2w7$sptk$u=nb%lv19@pznz$b#'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
